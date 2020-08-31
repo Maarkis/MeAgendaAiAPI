c7f43db302414f5cada805094dfbd780b2b6225a
Jean Markis <jeanmarkis85@gmail.com>
2020-08-30 23:42:46 -0300

    Adicionado -> Swagger e configurado

--- MeAgendaAi.Application/Startup.cs
+++ MeAgendaAi.Application/Startup.cs
@@ -1,15 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MeAgendaAi.Application
 {
@@ -26,6 +21,28 @@ namespace MeAgendaAi.Application
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Me agenda aí - API",
+                    Description = "API para gerenciamento da aplicação me agenda aí",
+                    TermsOfService = new Uri("https://github.com/Maarkis/MeAgendaAiAPI"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Jean Markis - Manoela Viana - Vitória Simões",
+                        Email = "jeanmarkis85@gmail.com;manoelanardy@gmail.com;vitoriasimoes_2000@outlook.com",
+                        Url = new Uri("https://github.com/Maarkis/MeAgendaAiAPI"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +55,13 @@ namespace MeAgendaAi.Application
 
             app.UseHttpsRedirection();
 
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Me Agenda Ai API - v1");
+            });
             app.UseRouting();
 
             app.UseAuthorization();
