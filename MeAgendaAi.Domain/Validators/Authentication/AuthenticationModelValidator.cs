817c02a979f3650575c7e58f440576ba3ce0e419
Jean Markis <jeanmarkis85@gmail.com>
2021-01-10 18:25:18 -0300

    Add Validation login

---
+++ MeAgendaAi.Domain/Validators/Authentication/AuthenticationModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MeAgendaAi.Domain.EpModels.User;
+using MeAgendaAi.Domain.Interfaces.Repositories;
+
+namespace MeAgendaAi.Domain.Validators.Authentication
+{
+    class AuthenticationModelValidator : AbstractValidator<LoginModel>
+    {
+        public AuthenticationModelValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                    .WithMessage("E-mail obrigátorio")
+                .EmailAddress()
+                    .WithMessage("Digite um e-mail válido");
+
+            RuleFor(x => x.Senha)
+                .NotEmpty()
+                    .WithMessage("A senha está vazia!");
+        }
+    }
+}
